63d93d3c94c127f632e009db8877439b20434121
Ben Howdle <benhowdle89@gmail.com>
2012-12-27 22:48:55 +0000

    t

--- pull.php
+++ pull.php
@@ -1,4 +1,5 @@
 <?php
-$output = exec('sudo git pull origin master');
+//$output = exec('sudo git pull origin master');
+$output = exec('git status');
 //$output = exec('touch test.txt');
 echo $output;
